54dbe9f8d1bc173da85953ae19d8b6e5a1b2b557
romeucampos <romeu.campos@hotmail.com>
2022-10-10 21:05:00 -0300

    add ipfs

---
+++ pages/api/uploadJSONToIPFS.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+const FormData = require('form-data');
+
+export const uploadJSONToIPFS = async(JSONBody) => {
+    const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
+    //making axios POST request to Pinata ⬇️
+    return axios 
+        .post(url, JSONBody, {
+            headers: {
+                // pinata_api_key: key,
+                // pinata_secret_api_key: secret,
+                authorization: "Bearer "+process.env.REACT_APP_PINATA_JWT
+            }
+        })
+        .then(function (response) {
+           return {
+               success: true,
+               pinataURL: "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash
+           };
+        })
+        .catch(function (error) {
+            console.log(error)
+            return {
+                success: false,
+                message: error.message,
+            }
+
+    });
+};
+
