91748ba9aea4201c5877470af4e22c5c92e60cfc
Gustavo <gustavo@obex.pw>
2022-10-13 10:15:36 -0300

    change pinata path

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -25,14 +25,12 @@ const post = async (req, res) => {
 };
 
 const sendIpfs = async (file: formidable.File): Promise<string> => {
-  const res = await pinata.pinFileToIPFS(fs.createReadStream(file.filepath));
-
+  const res = await pinata.pinFromFS(file.filepath);
   return res.IpfsHash;
 };
 
 const sendJson = async (data: Object): Promise<string> => {
   const res = await pinata.pinJSONToIPFS(data);
-
   return res.IpfsHash;
 };
 
